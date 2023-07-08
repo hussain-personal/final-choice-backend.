79db29afebfd895e98d4c48d24b4ebf21c5c1e35
Hussain rizvi <hussainrizvi486@gmail.com>
2023-07-09 00:09:09 +0500

    connected with database

--- server/settings.py
+++ server/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'HOST': 'containers-us-west-4.railway.app',
+        'USER': 'root',
+        'PASSWORD': 'RdpdwOfz6LaNyV98cdEC',
+        'PORT': '5848',
     }
 }
 
